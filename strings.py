85a8a9f6b5fb7ffb797cf046545467dc882d2097
abhinav9444 <abhinavkrsingh9444@gmail.com>
2026-04-29 13:21:19 +0530

    Python Basics

---
+++ strings.py
@@ -0,0 +1,19 @@
+# Strings - Working with Textual Data
+message = "Hello Abhinav"
+
+# Printing String Variables 
+print(message)
+
+# Length of the String
+print(len(message))
+
+# Accessing String using Indexes (Positive Indexing or Zero Based Indexing)
+print(message[1])
+
+# Accessing String using Indexes (Negative Indexing)
+print(message[-2])
+
+# Accessing String using Range of Indexes
+print(message[0:5])
+print(message[:5])
+print(message[6:])
